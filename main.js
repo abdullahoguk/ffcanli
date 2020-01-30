fb03e39e05920f9278b23fa4610c6152bc3475f4
Abdullah <abdullahoguk@gmail.com>
2020-01-30 20:04:04 +0300

    list filter:search by query

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ import {default as allPlayers} from './data/json/oyuncular/all.js';
 import {default as points} from './data/json/puanlar/puanlar.js';
 import {default as teams} from './data/json/takimlar.js';
 
+//DOM elements
+var resultsDOM = document.querySelector(".searchResults");
+var searchInput = document.querySelector("input#search");
 
-var resultsDOM = document.querySelector(".searchResults")
-
+//data elements
 var players = Object.entries(allPlayers);
 
 var userTeam={
@@ -25,14 +27,19 @@ var positions={
     "f": "Forvet",
     "y": "Yedek"
 }
-//console.log(allPlayers);
 
+
+/*------------------------------
+            MAIN WORKFLOW
+-----------------------------*/
 
 //Inıtial Render
 resultsDOM.innerHTML = renderAllPlayers();
 
+
 function renderAllPlayers(){
-    var resultHTML="";
+    var resultHTML= ""; 
+
     players.forEach(function (team){
         var teamName=team[0];
         var teamPlayers = Object.entries(team[1]);
@@ -46,9 +53,29 @@ function renderAllPlayers(){
 }
 
 
+/*-----------------------------------
+                Event Listeners 
+------------------------------------*/
+searchInput.addEventListener("input",filterResults);
+function filterResults(){
+    var re = new RegExp(this.value,"gi");
+    var list = document.querySelectorAll(".searchResults > .playerItem");
+    list.forEach(function(player){
+        if(re.test(player.dataset.name)){
+            if(player.classList.contains("hidden")){ player.classList.remove("hidden")};
+        }
+        else{
+            if(!player.classList.contains("hidden")){ player.classList.add("hidden")};
+        }
+    })
+}
+
+/*-----------------------------------
+                Helper Functions
+------------------------------------*/
 function createResultItem(id, team, name, position, point){
     return `
-    <div class="playerItem" data-id=${id} data-team=${team} data-position=${position}>
+    <div class="playerItem" data-id=${id} data-team=${team} data-position=${position} data-name=${name}>
         <span class="team">${team}</span>
         <span class="name">${name}</span>
         <span class="position">${positions[position]}</span>
