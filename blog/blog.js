f87aa288be2270bbe96868504ee5c9f2d47a416b
Abdullah <abdullahoguk@gmail.com>
2020-03-07 04:28:21 +0300

    son haberler(blog)

---
+++ blog/blog.js
@@ -0,0 +1,52 @@
+//import documentToHtmlString as richTextToHTML "https://unpkg.com/@contentful/rich-text-html-renderer@13.4.0/dist/rich-text-html-renderer.es5.js"
+
+var content = document.querySelector(".feed");
+content.classList.add("ui","items")
+var dataURL =
+	"https://cdn.contentful.com/spaces/blfw5f11e921/environments/master/entries?access_token=5Kg3FYTOA0My4ZXtgfI_E6shQHGAsEyQRYOdS5CeHvg";
+
+main();
+
+async function main() {
+	//var richTextToHTML;
+	/*var documentToHtmlString = await import("https://unpkg.com/@contentful/rich-text-html-renderer@13.4.0/dist/rich-text-html-renderer.es5.js")
+  .then(module => {
+        richTextToHTML = module.documentToHtmlString;
+      });*/
+
+	var data = {};
+
+	await loadJSONAsync(dataURL)
+		.then(json => {
+			data = json;
+		})
+		.catch(reason => console.log(`JSON okunurken hata: ${reason.message}`));
+
+	await data.items.forEach(function(item) {
+		var feedItem = document.createElement("div");
+		feedItem.classList.add("item");
+
+    feedItem.innerHTML = `
+    <div class="ui small image">
+      <img src=${item.fields.imageurl}>
+    </div>
+    <div class="middle aligned content">
+      <div class="header">
+        ${item.fields.title}
+      </div>
+      <div class="detail">
+        ${documentToHtmlString(item.fields.detail)}
+      </div>
+      </div>
+    </div>
+  
+  `
+		content.appendChild(feedItem);
+	});
+}
+
+async function loadJSONAsync(url) {
+	let response = await fetch(url);
+	let data = await response.json();
+	return data;
+}
