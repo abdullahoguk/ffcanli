682ecb69ee45d82630f1363ea626152eed8c41f1
Abdullah <abdullahoguk@gmail.com>
2020-09-27 12:36:02 +0300

    blog api 1

---
+++ api/blog.js
@@ -0,0 +1,36 @@
+//import documentToHtmlString as richTextToHTML "https://unpkg.com/@contentful/rich-text-html-renderer@13.4.0/dist/rich-text-html-renderer.es5.js"
+
+var content = document.querySelector("body");
+var dataURL =
+	"https://cdn.contentful.com/spaces/blfw5f11e921/environments/master/entries?access_token=5Kg3FYTOA0My4ZXtgfI_E6shQHGAsEyQRYOdS5CeHvg&order=sys.createdAt";
+
+main();
+
+async function main() {
+	//var richTextToHTML;
+	/*var documentToHtmlString = await import("https://unpkg.com/@contentful/rich-text-html-renderer@13.4.0/dist/rich-text-html-renderer.es5.js")
+  .then(module => {
+        richTextToHTML = module.documentToHtmlString;
+      });*/
+
+	var data = {};
+
+	await loadJSONAsync(dataURL)
+		.then(json => {
+			data = json;
+		})
+		.catch(reason => console.log(`JSON okunurken hata: ${reason.message}`));
+
+	await data.items.reverse().forEach(function(item) {
+		
+    item.fields.detail = documentToHtmlString(item.fields.detail)
+  });
+  content.innerText = JSON.stringify(data);
+
+}
+
+async function loadJSONAsync(url) {
+	let response = await fetch(url);
+	let data = await response.json();
+	return data;
+}
